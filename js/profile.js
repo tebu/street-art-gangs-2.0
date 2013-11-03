1edfb11ab71af0b1f2666475ab1b3e60f534b19b
Daniele Zanni <syymza@gmail.com>
2013-11-03 20:17:40 +0200

    Check that gangster id exists when loading a page

--- js/profile.js
+++ js/profile.js
@@ -1,7 +1,7 @@
       //Load venues or redirect
       jQuery(document).ready(function(){
 
-      if (!localStorage.authorization||!localStorage.color) {
+      if (!localStorage.authorization||!localStorage.color||!localStorage.gangster) {
         window.location.replace("splash.html");
       } else {
 
