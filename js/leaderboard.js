bdba831862581945ebfbe4d5e7c68cb237e6cf32
Daniele Zanni <syymza@gmail.com>
2013-11-05 10:51:18 +0200

    Added players leaderboard

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -12,8 +12,11 @@
     $('body').removeClass().addClass(color);
 
     var endpoint = "http://vm0063.virtues.fi/gangs/";
+    var endpoint2 = "http://vm0063.virtues.fi/gangsters/";
+
     var authorization=localStorage.authorization;
 
+//Load Gangs Leaderboard
 
     $.ajax({
       type: "GET",
@@ -35,14 +38,36 @@
         table.append(line);
       };
 
-
-
     }).fail(function( jqXHR, textStatus ) {
     //TODO fix this
       alert("Error: something went wrong while loading the profile: "+ textStatus);
     });
 
 
+    $.ajax({
+      type: "GET",
+      url: endpoint2,
+      dataType: 'json',
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader ("Authorization", authorization);
+      }
+    }).done(function( data ) {
+
+      var table = $('#player-board');
+      //TODO: limit number of lines and select you
+      for (var i = 0; i < data.length; i++) {
+        var line = $("<tr>");
+        $("<td>").text(i+1).appendTo(line);
+        $("<td>").text(data[i].username).appendTo(line);
+        $("<td>").text(643636).appendTo(line); //TODO: insert tags created
+        $("<td>").text(43643).appendTo(line); //TODO: insert points
+        table.append(line);
+      };
+
+    }).fail(function( jqXHR, textStatus ) {
+    //TODO fix this
+      alert("Error: something went wrong while loading the profile: "+ textStatus);
+    });
 
     $('#leaderboards').liquidSlider({
       slideEaseDuration: 500,
