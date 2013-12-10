fa8c75114f687d809ecf789809543ae0a4b4a982
Plij <paalaves@gmail.com>
2013-12-10 12:35:21 +0200

    mixpanel

--- js/index.js
+++ js/index.js
@@ -4,7 +4,8 @@ jQuery(document).ready(function(){
       if (!localStorage.authorization||!localStorage.color||!localStorage.gangster||!localStorage.gang) {
         window.location.replace("splash.html");
       } else {
-		mixpanel.track("PageLaunch", {page:"index"});
+		var gangster = localStorage.gangster;
+		mixpanel.track("PageLaunch", {page:"index", gangster: gangster});
         //Change color background depending on player's color
         var color = localStorage.color;
         $('body').removeClass().addClass(color)
