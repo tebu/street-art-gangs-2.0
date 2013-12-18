67a9d3c55af4af8a7b68510892c565a3c01ee1cd
Plij <paalaves@gmail.com>
2013-12-18 13:42:16 +0200

    Fix

--- js/spraying.js
+++ js/spraying.js
@@ -6,15 +6,9 @@ jQuery(document).ready(function(){
       } else {
 	  	var color = localStorage.color;
 		var gangster = localStorage.gangster;
-<<<<<<< HEAD
 
-		mixpanel.track("PageLaunch", {page:"spraying", gang: color, gangster: gangster});
-
-=======
-	
 		mixpanel.track("PageLaunch", {page:"spraying", gang: color, gangster: gangster}); 
-				
->>>>>>> origin/mixPanel
+
         //Change color background depending on player's color
         $('body').removeClass().addClass(color)
         $("#can-spraying").attr("src", "img/can-"+color+".png" );
@@ -88,7 +82,6 @@ var registerSpray = function() {
         //var venue = localStorage.venue;
         var venue = 26; //TODO change with stored venue <---------------------------------!
         var endpoint = "http://vm0063.virtues.fi/venues/"+venue+"/";
-        var now = moment().format();
         var data =  {
                 gangster: gangster,
                 latestEditTimestamp: now
