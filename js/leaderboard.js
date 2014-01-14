7ee9475bd3ae6579fdd91b7c67ca05642485fc8e
Plij <paalaves@gmail.com>
2014-01-14 15:52:13 +0200

    Points and Tags for GangLeaderboard

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -14,7 +14,7 @@
     //Change color background depending on player's color
     $('body').removeClass().addClass(color);
 
-    var endpoint = "http://vm0063.virtues.fi/gangs/";
+    /*var endpoint = "http://vm0063.virtues.fi/gangs/"; */   //Rejected the use of gang data for now, for the lack of points there
     var endpoint2 = "http://vm0063.virtues.fi/gangsters/";
 
     var authorization=localStorage.authorization;
@@ -23,7 +23,7 @@
 
     $.ajax({
       type: "GET",
-      url: endpoint,
+      url: endpoint2,
       dataType: 'json',
       beforeSend: function (xhr) {
         xhr.setRequestHeader ("Authorization", authorization);
@@ -31,16 +31,22 @@
     }).done(function( data ) {
 
       var table = $('#gang-board');
-
-      for (var i = 0; i < data.length; i++) {
+	  
+	   for (var i = 0; i < 3; i++) {  //1=Purple, 2=Blue and 3=Green, Apply only here, not equal to "gang" in gangsters database 
+	    var gangNumber = i+1;
+		var gangName = functionName(i);
+		var gangTagsPoints = functionTags (i, data);
+		
         var line = $("<tr>");
         $("<td>").text(i+1).appendTo(line);
-        $("<td>").text(data[i].name).appendTo(line);
-        $("<td>").text(643636).appendTo(line); //TODO: insert tags created
-        $("<td>").text(43643).appendTo(line); //TODO: insert points
+        $("<td>").text(gangName).appendTo(line);
+        $("<td>").text(gangTagsPoints[0]).appendTo(line); //TODO: insert tags created
+        $("<td>").text(gangTagsPoints[1]).appendTo(line); //TODO: insert points
         table.append(line);
       };
 
+	  
+		
 //Load Players Leaderboard
 
       $.ajax({
@@ -98,4 +104,36 @@
      $('th').addClass('animated slideInRight');
      $('td').addClass('animated slideInLeft');
    }
+   
+   function functionName(counterValueGang){ //Gang Names by counter value
+			counterValueGang++;
+			if (counterValueGang == 1) {
+			gangName = "Purple Knights";
+			}else if (counterValueGang == 2){
+			gangName = "Blue Angels";
+			}else{ 
+			gangName = "Green Shamans";
+			}
+   return gangName;
+   }
+   
+	function functionTags(counterValueGang, data){ //Gang tags by gangsters 
+			counterValueGang++;
+			var gangTags = 0;
+			var gangPoints = 0;
+			for (var i = data.length - 1; i >= 0; i--) {
+			if (counterValueGang == 1 && data[i].color == "purple") {
+			gangTags += data[i].tags_created;   //points and tags for team purple		
+			gangPoints += data[i].points;
+			}else if (counterValueGang == 2 && data[i].color == "blue"){
+			gangTags += data[i].tags_created; //points and tags for team blue
+			gangPoints += data[i].points;
+			}else if (counterValueGang == 3 && data[i].color == "green"){ 
+			gangTags += data[i].tags_created;//points and tags for team green
+			gangPoints += data[i].points;
+			}
+			}
+   return [gangTags,gangPoints];
+   }
+   
  });
